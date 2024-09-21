c89f5b16b2d3ed62d8c8584146134f03f58eadb8
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-09-21 12:40:01 +1000

    fix: react native errors

--- frontend/app/(private)/_layout.tsx
+++ frontend/app/(private)/_layout.tsx
@@ -1,8 +1,6 @@
-import { Stack } from 'expo-router';
-import { Tabs } from 'expo-router';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-
+import { Tabs } from "expo-router";
+import Icon from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function PrivateLayout() {
   return (
@@ -10,23 +8,23 @@ export default function PrivateLayout() {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ color, size }) => {
           let iconName;
-          if (route.name === 'dashboard') {
-            iconName = 'home';
-          } else if (route.name === 'friends') {
-            iconName = 'users';
-          } else if (route.name === 'you') {
-            iconName = 'user';
+          if (route.name === "dashboard") {
+            iconName = "home";
+          } else if (route.name === "friends") {
+            iconName = "users";
+          } else if (route.name === "you") {
+            iconName = "user";
           }
           return <Icon name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: '#fefefe',
-        tabBarInactiveTintColor: '#000000',
+        tabBarActiveTintColor: "#fefefe",
+        tabBarInactiveTintColor: "#000000",
         tabBarStyle: {
-          backgroundColor: '#fc8a00', // Translucent white background
-          borderTopColor: 'rgba(255, 255, 255, 0.2)', // Slight border effect
+          backgroundColor: "#fc8a00", // Translucent white background
+          borderTopColor: "rgba(255, 255, 255, 0.2)", // Slight border effect
           paddingBottom: 20, // Padding for the bottom icons
           paddingTop: 10, // Padding for the top icons
-          position: 'absolute', // Make the tab bar overlay content
+          position: "absolute", // Make the tab bar overlay content
           zIndex: 10, // Ensure it's layered on top
           bottom: 0,
           left: 0,
@@ -42,9 +40,10 @@ export default function PrivateLayout() {
         },
       })}
     >
-      <Tabs.Screen name="dashboard" options={{ title: 'Dashboard' }} />
-      <Tabs.Screen name="friends" options={{ title: 'Friends' }} />
-      <Tabs.Screen name="you" options={{ title: 'You' }} />
+      <Tabs.Screen name="dashboard" options={{ title: "Dashboard" }} />
+      <Tabs.Screen name="friends" options={{ title: "Friends" }} />
+      <Tabs.Screen name="you" options={{ title: "You" }} />
     </Tabs>
   );
 }
+
--- frontend/app/(public)/_layout.tsx
+++ frontend/app/(public)/_layout.tsx
@@ -1,5 +1,5 @@
 // (public)/_layout.tsx
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
 export default function PublicLayout() {
   return (
@@ -7,9 +7,6 @@ export default function PublicLayout() {
       {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
       <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="signup" options={{ headerShown: false }} />
-      <Stack.Screen name="add-friend" options={{ headerShown: false }} />
-      <Stack.Screen name="friend-requests" options={{ headerShown: false }} />
-      <Stack.Screen name="edit-profile" options={{ headerShown: false }} />
     </Stack>
   );
 }
