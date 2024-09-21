2bc15241e505a1874b35dd30cefabe57c59107ed
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-09-21 15:22:31 +1000

    feat: implement all users search for add friend

--- frontend/app/(private)/dashboard.tsx
+++ frontend/app/(private)/dashboard.tsx
@@ -1,63 +1,77 @@
-import React, { useLayoutEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
-import { useRouter } from 'expo-router';
-import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useLayoutEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function DashboardPage() {
   const router = useRouter();
   const navigation = useNavigation();
 
   const [friends, setFriends] = useState([
-    { id: 1, name: 'John Doe', score: 150 },
-    { id: 2, name: 'Jane Smith', score: 200 },
-    { id: 3, name: 'Robert Johnson', score: 120 },
-    { id: 4, name: 'Emily Davis', score: 180 },
-    { id: 5, name: 'Usain Bolt', score: 300 },
-    { id: 6, name: 'Lionel Messi', score: 250 },
-    { id: 7, name: 'Serena Williams', score: 220 },
-    { id: 8, name: 'LeBron James', score: 170 },
-    { id: 9, name: 'Trent Boult', score: 121 },
-    { id: 10, name: 'CR7', score: 240 },
-    { id: 11, name: 'Novak Djokovic', score: 320 },
-    { id: 12, name: 'MJ', score: 340 },
-    { id: 13, name: 'Venus Williams', score: 120 },
-    { id: 14, name: 'Virat Kohli', score: 370 },
+    { id: 1, name: "John Doe", score: 150 },
+    { id: 2, name: "Jane Smith", score: 200 },
+    { id: 3, name: "Robert Johnson", score: 120 },
+    { id: 4, name: "Emily Davis", score: 180 },
+    { id: 5, name: "Usain Bolt", score: 300 },
+    { id: 6, name: "Lionel Messi", score: 250 },
+    { id: 7, name: "Serena Williams", score: 220 },
+    { id: 8, name: "LeBron James", score: 170 },
+    { id: 9, name: "Trent Boult", score: 121 },
+    { id: 10, name: "CR7", score: 240 },
+    { id: 11, name: "Novak Djokovic", score: 320 },
+    { id: 12, name: "MJ", score: 340 },
+    { id: 13, name: "Venus Williams", score: 120 },
+    { id: 14, name: "Virat Kohli", score: 370 },
   ]);
 
   const [showRequests, setShowRequests] = useState(false);
   const [friendRequests, setFriendRequests] = useState([
-    { id: 1, name: 'Michael Jordan' },
-    { id: 2, name: 'Novak Djokovic' },
-    { id: 3, name: 'Virat Kohli' },
-    { id: 4, name: 'Leo Messi' },
-    { id: 5, name: 'Roger Federer' },
+    { id: 1, name: "Michael Jordan" },
+    { id: 2, name: "Novak Djokovic" },
+    { id: 3, name: "Virat Kohli" },
+    { id: 4, name: "Leo Messi" },
+    { id: 5, name: "Roger Federer" },
   ]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
-      headerTitle: 'My Dashboard',
+      headerTitle: "My Dashboard",
       headerTransparent: true,
       headerStyle: {
-        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        backgroundColor: "rgba(0, 0, 0, 0.3)",
       },
-      headerTintColor: '#000',
+      headerTintColor: "#000",
       headerTitleStyle: {
-        fontWeight: 'bold',
+        fontWeight: "bold",
         fontSize: 24,
       },
       headerRight: () => (
         <View style={styles.headerIcons}>
-          <TouchableOpacity onPress={() => setShowRequests(!showRequests)} style={styles.headerButton}>
+          <TouchableOpacity
+            onPress={() => setShowRequests(!showRequests)}
+            style={styles.headerButton}
+          >
             <Icon name="bell" size={24} color="#fc8a00" />
             {friendRequests.length > 0 && (
               <View style={styles.notificationBadge}>
-                <Text style={styles.notificationText}>{friendRequests.length}</Text>
+                <Text style={styles.notificationText}>
+                  {friendRequests.length}
+                </Text>
               </View>
             )}
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.replace('/')} style={styles.headerButton}>
+          <TouchableOpacity
+            onPress={() => router.replace("/")}
+            style={styles.headerButton}
+          >
             <Icon name="sign-out" size={24} color="#fc8a00" />
           </TouchableOpacity>
         </View>
@@ -69,13 +83,13 @@ export default function DashboardPage() {
     const acceptedRequest = friendRequests.find((req) => req.id === requestId);
     setFriends((prevFriends) => [...prevFriends, acceptedRequest]);
     setFriendRequests((prevRequests) =>
-      prevRequests.filter((req) => req.id !== requestId)
+      prevRequests.filter((req) => req.id !== requestId),
     );
   };
 
   const handleRejectRequest = (requestId) => {
     setFriendRequests((prevRequests) =>
-      prevRequests.filter((req) => req.id !== requestId)
+      prevRequests.filter((req) => req.id !== requestId),
     );
   };
 
@@ -90,9 +104,7 @@ export default function DashboardPage() {
   return (
     <View style={styles.container}>
       <View style={styles.overlay}>
-        <Text style={styles.welcomeText}>Welcome to your Dashboard!</Text>
-        
-        <Text style={styles.leaderboardTitle}>Leaderboard</Text>
+        <Text style={styles.leaderboardTitle}>Global Leaderboard</Text>
         <FlatList
           data={friends.sort((a, b) => b.score - a.score)}
           renderItem={renderLeaderboardCard}
@@ -125,7 +137,7 @@ export default function DashboardPage() {
             </View>
           ))}
           {friendRequests.length > 4 && (
-            <TouchableOpacity onPress={() => router.push('/friend-requests')}>
+            <TouchableOpacity onPress={() => router.push("/friend-requests")}>
               <Text style={styles.showMoreLink}>Show More</Text>
             </TouchableOpacity>
           )}
@@ -137,56 +149,56 @@ export default function DashboardPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fefefe',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#fefefe",
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: 110,
     paddingBottom: 100,
   },
   overlay: {
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   welcomeText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#000',
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: "#000",
+    textAlign: "center",
     marginBottom: 20,
   },
   headerButton: {
     marginRight: 15,
   },
   headerIcons: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   notificationBadge: {
-    position: 'absolute',
+    position: "absolute",
     right: -6,
     top: -3,
-    backgroundColor: 'red',
+    backgroundColor: "red",
     width: 18,
     height: 18,
     borderRadius: 9,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   notificationText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 12,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   dropdown: {
-    position: 'absolute',
-    top: 100, 
+    position: "absolute",
+    top: 100,
     right: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 10,
     borderRadius: 5,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
@@ -196,92 +208,92 @@ const styles = StyleSheet.create({
   },
   dropdownHeader: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
-    color: '#000',
+    color: "#000",
   },
   requestContainer: {
     marginBottom: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
     paddingBottom: 10,
   },
   dropdownText: {
-    color: '#000',
+    color: "#000",
     paddingVertical: 5,
     fontSize: 16,
   },
   requestButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 5,
   },
   acceptButton: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     padding: 5,
     borderRadius: 5,
     marginRight: 10,
   },
   rejectButton: {
-    backgroundColor: 'red',
+    backgroundColor: "red",
     padding: 5,
     borderRadius: 5,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 14,
   },
   showMoreLink: {
-    color: '#e9902c',
+    color: "#e9902c",
     fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginTop: 10,
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
   },
   leaderboardTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
-    color: '#fc8a00',
+    color: "#fc8a00",
   },
   leaderboardCard: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
     padding: 10,
     marginVertical: 5,
-    width: '100%', // Adjust width
+    width: "100%", // Adjust width
     borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
     elevation: 4,
-    alignSelf: 'center', // Center the card
+    alignSelf: "center", // Center the card
   },
   leaderboardRank: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000',
+    fontWeight: "bold",
+    color: "#000",
     width: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
   leaderboardName: {
     fontSize: 16,
-    color: '#333',
+    color: "#333",
     flex: 1,
-    textAlign: 'left',
+    textAlign: "left",
     marginLeft: 10,
   },
   leaderboardScore: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#fc8a00',
+    fontWeight: "bold",
+    color: "#fc8a00",
   },
   leaderboardList: {
-    width: '100%', // Set this to 100%
-    alignItems: 'center', // Center content horizontally
+    width: "100%", // Set this to 100%
+    alignItems: "center", // Center content horizontally
   },
 });
--- frontend/app/add-friend.tsx
+++ frontend/app/add-friend.tsx
@@ -1,14 +1,19 @@
-import React, { useLayoutEffect, useState } from 'react';
-import { View, TextInput, FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import {
+  View,
+  TextInput,
+  FlatList,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { LOCALHOST } from "./constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function AddFriendsPage() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [availableUsers, setAvailableUsers] = useState([
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Smith' },
-    // Add more users as needed
-  ]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [availableUsers, setAvailableUsers] = useState([]);
 
   const navigation = useNavigation();
 
@@ -16,24 +21,58 @@ export default function AddFriendsPage() {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
-      headerTitle: 'Add Friends',
+      headerTitle: "Add Friends",
       headerTransparent: false,
-      headerBackTitle: 'Friends',
-      headerTintColor: '#000',
+      headerBackTitle: "Friends",
+      headerTintColor: "#000",
       headerTitleStyle: {
-        fontWeight: 'bold',
+        fontWeight: "bold",
         fontSize: 24,
       },
     });
   }, [navigation]);
 
-  const filteredUsers = availableUsers.filter(user =>
-    user.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const authToken = await getAuthToken(); // Implement your method to get the auth token
+        const response = await fetch(
+          "http://" + LOCALHOST + ":3000/api/v1/users",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${authToken}`, // Include your auth token in the header
+            },
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+
+        const data = await response.json();
+        setAvailableUsers(data); // Adjust according to your API response structure
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const filteredUsers = availableUsers.filter((user) => {
+    const combinedSearchField = user.name + user.username + user.email;
+    return combinedSearchField
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+  });
 
   const renderUser = ({ item }) => (
     <TouchableOpacity style={styles.card}>
-      <Text style={styles.userName}>{item.name}</Text>
+      <Text style={styles.userName}>{item.username}</Text>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.email}>{item.email}</Text>
     </TouchableOpacity>
   );
 
@@ -59,37 +98,59 @@ export default function AddFriendsPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fefefe',
+    backgroundColor: "#fefefe",
     padding: 20,
   },
   searchBar: {
     height: 40,
-    borderColor: '#e9902c',
+    borderColor: "#e9902c",
     borderWidth: 1,
     borderRadius: 8,
     marginBottom: 20,
     paddingHorizontal: 10,
-    color: '#000', // Ensures text entered is visible
+    color: "#000", // Ensures text entered is visible
   },
   card: {
-    backgroundColor: '#e9902c',
+    backgroundColor: "#e9902c",
     padding: 15,
     marginVertical: 8,
     borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 3.84,
     elevation: 5, // Ensures shadow is visible on Android
-    flexDirection: 'row', // Ensures content inside the card is aligned properly
-    alignItems: 'center', // Centers text vertically within the card
+    flexDirection: "column", // Ensures content inside the card is aligned properly
+    alignItems: "start", // Centers text vertically within the card
+    justifyContent: "center", // Centers text vertically within the card
   },
   userName: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 18,
   },
+  email: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  name: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+
   listContent: {
     paddingBottom: 20,
   },
 });
+
+const getAuthToken = async () => {
+  try {
+    const token = await AsyncStorage.getItem("authToken");
+    return token; // Will return null if the token is not found
+  } catch (error) {
+    console.error("Error retrieving auth token:", error);
+    return null; // Return null in case of an error
+  }
+};
